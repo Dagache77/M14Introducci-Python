f44d5832b2de48cdf0e0d3234e1ec5c52013a48f
Dagache77 <158280358+Dagache77@users.noreply.github.com>
2024-02-01 14:06:52 +0100

    Update and rename Numero-Factorial to Numero-Factorial.py

---
+++ Numero-Factorial.py
@@ -0,0 +1,8 @@
+num = int(input("Digues un número per calcular el seu valor factorial: "))
+resultat = 1
+while num > 0:
+        resultat = resultat * num
+        num = num -1
+print("El Factorial és", resultat)
+
+
