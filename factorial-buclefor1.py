451a41ebea051fe5cf126931d6124dee39e4883a
Dagache77 <158280358+Dagache77@users.noreply.github.com>
2024-02-05 13:58:39 +0100

    Update factorial-buclefor1.py

--- factorial-buclefor1.py
+++ factorial-buclefor1.py
@@ -1,1 +1,5 @@
-
+factorial = int(input("Digues un número per calcular el seu valor factorial: "))
+resultat = 1
+for i in range(1,factorial+1):
+    resultat = resultat * i
+print("El resultat és", resultat)
