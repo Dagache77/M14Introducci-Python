cb1cfbeee0ec1a3f26a92d05e835b01d375df87d
Dagache77 <158280358+Dagache77@users.noreply.github.com>
2024-02-06 23:50:58 +0100

    Create bateria2-10.py

---
+++ bateria2-10.py
@@ -0,0 +1,6 @@
+anys_experiencia = int(input("Quant temps portes treballant com a programador? "))
+
+if anys_experiencia < 5:
+    print("Ets un programador junior.")
+else:
+    print("Ets un programador senior.")
