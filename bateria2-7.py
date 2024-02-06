22ff320d4b2ce1c0308616beec82ba260ed9aea3
Dagache77 <158280358+Dagache77@users.noreply.github.com>
2024-02-06 23:46:26 +0100

    Create bateria2-7.py

---
+++ bateria2-7.py
@@ -0,0 +1,8 @@
+numero_usuario = float(input("Ingrese un número: "))
+
+if numero_usuario > 100:
+    print("El número ingresado es mayor que 100.")
+elif numero_usuario < 100:
+    print("El número ingresado es menor que 100.")
+else:
+    print("El número ingresado es igual a 100.")
