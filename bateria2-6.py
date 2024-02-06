94a0f78b5349a0e68d931fd2d74cbdecfe3d83fa
Dagache77 <158280358+Dagache77@users.noreply.github.com>
2024-02-06 23:44:48 +0100

    Create bateria2-6.py

---
+++ bateria2-6.py
@@ -0,0 +1,6 @@
+nom_usuari = input("Introdueix el teu nom: ")
+
+if nom_usuari == "Amilcar" or nom_usuari == "Sila":
+    print(f"Benvingut, {nom_usuari}!")
+else:
+print("Nom d'usuari no vàlid. Accés no autoritzat.")
