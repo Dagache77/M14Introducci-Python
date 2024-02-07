e68d7e4cccd59966eccdad2b7de04a90239e5985
Dagache77 <158280358+Dagache77@users.noreply.github.com>
2024-02-07 08:44:37 +0100

    Update multiplicaciónumero.py

--- multiplicaciónumero.py
+++ multiplicaciónumero.py
@@ -1,5 +1,4 @@
 numero = int(input("Introdueix un numero: "))
-resultat = numero
-for i in range(1,11):
+for i in range(1, 11):
     resultat = numero * i
-    print(resultat)
+    print(f"{numero} x {i} = {resultat}")
