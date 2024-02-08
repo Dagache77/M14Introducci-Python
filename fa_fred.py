1b2139aea8001cc37667ff49c5b7405ab01f7d99
Dagache77 <158280358+Dagache77@users.noreply.github.com>
2024-02-08 13:39:55 +0100

    Create fa_fred.py

---
+++ fa_fred.py
@@ -0,0 +1,10 @@
+# Programa Fa fred
+
+# Demana a l'usuari la temperatura
+temperatura = float(input("Quina és la temperatura actual en graus Celsius? "))
+
+# Verifica si fa fred (menys de 15 graus)
+if temperatura < 15:
+    print("Agafa una jaqueta, que refresca.")
+else:
+    print("Quin temps més bo que fa.")
