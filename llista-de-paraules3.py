3e79c42293ba74b2d4208d2fa9e65339cf04d078
Dagache77 <158280358+Dagache77@users.noreply.github.com>
2024-02-08 13:28:19 +0100

    Update llista-de-paraules3.py

--- llista-de-paraules3.py
+++ llista-de-paraules3.py
@@ -1,8 +1,8 @@
 concatenar = ""
 while True:
     lletra = input("Introdueix una paraula o final per acabar: ")
-    if "final" im lletra:
+    if "final" in lletra:
         break
     else:
-        concatenar = concatenar + " "
+        concatenar = (concatenar+ lletra + " ")
 print(concatenar)
