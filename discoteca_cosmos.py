9d0530af1420c91d03155a2e84a26c18ba4a71ea
Dagache77 <158280358+Dagache77@users.noreply.github.com>
2024-02-08 13:36:47 +0100

    Create discoteca_cosmos.py

---
+++ discoteca_cosmos.py
@@ -0,0 +1,10 @@
+# Programa Discoteca Cosmos
+
+# Demana a l'usuari la seva edat
+edat = int(input("Quina és la teva edat? "))
+
+# Verifica si l'usuari és major d'edat
+if edat >= 18:
+    print("Pots entrar a la Discoteca Cosmos. Gaudeix de la nit!")
+else:
+    print("Ho sentim, no pots entrar a la Discoteca Cosmos. Torna quan siguis major d'edat.")
