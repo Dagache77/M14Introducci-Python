713118c90eb44fe42f9432428e853e3a79e3912c
Dagache77 <158280358+Dagache77@users.noreply.github.com>
2024-02-15 13:58:33 +0100

    Create comunitat.py

---
+++ comunitat.py
@@ -0,0 +1,12 @@
+comunitat = ["Gandalf", "Legolas", "Gimli","Aragorn","Merry","Pippin","Frodo","Sam","Boromir"]
+print(comunitat[0])
+print(comunitat[len(comunitat)-1])
+print(comunitat[int(len(comunitat) /2)])
+comunitat.sort()
+print(comunitat[0])
+print(comunitat[-1])
+comunitat.remove("Aragorn")
+print(comunitat[0])
+comunitat.append("Arwen")
+comunitat.sort()
+print(comunitat)
